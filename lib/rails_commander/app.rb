5e9ddf6fa3c3d3172056ee1494320b8dbab39f15
Bartek Wilczek <bartek.wilczek@toptal.com>
2019-09-08 00:03:28 +0200

    Improve configurability

--- lib/rails_commander/app.rb
+++ lib/rails_commander/app.rb
@@ -14,28 +14,51 @@ module RailsCommander
     end
 
     def start
+      return if running?
+
       Dir.chdir(@path) do
         @pid = Process.spawn(
+          config.env_vars,
           config.cmd_start,
           unsetenv_others: config.unset_env_vars,
-          %i[out err] => [config.log_path, 'w']
+          out: [config.out_path, 'w'],
+          err: [config.err_path, 'w']
         )
+      end
+    end
+
+    def running?
+      return false unless pid
+
+      begin
+        Process.getpgid(pid)
+        true
+      rescue Errno::ESRCH
+        false
       end
     end
 
     def stop
+      return unless running?
+
       Process.kill('TERM', @pid)
     end
 
-    def db_reset
+    def task(task)
       Dir.chdir(@path) do
         pid = Process.spawn(
-          'bundle exec ./bin/rails db:reset',
+          config.env_vars,
+          "bundle exec ./bin/rails #{task}",
           unsetenv_others: config.unset_env_vars,
-          %i[out err] => [config.log_path, 'w']
+          out: [config.out_path, 'w'],
+          err: [config.err_path, 'w']
         )
         Process.wait(pid)
       end
+    end
+
+    def db_reset
+      task('db:reset')
     end
   end
 end
--- lib/rails_commander/config.rb
+++ lib/rails_commander/config.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 module RailsCommander
-  Config = Struct.new(:log_path, :cmd_start, :port, :pidfile, :env, :unset_env_vars, keyword_init: true) do
+  Config = Struct.new(:out_path, :err_path, :cmd_start, :port, :pidfile, :env_vars, :rails_env, :unset_env_vars, keyword_init: true) do
     def initialize(*)
       super
       self.pidfile ||= './tmp/pids/server.pid'
-      self.log_path ||= '/tmp/rails_commander.log'
-      self.env ||= 'development'
+      self.out_path ||= '/tmp/rails_commander_out.log'
+      self.err_path ||= '/tmp/rails_commander_err.log'
+      self.rails_env ||= 'development'
       self.port ||= 3000
       self.unset_env_vars ||= true
-      self.cmd_start ||= "bundle exec ./bin/rails s --port=#{self.port} --pid=#{self.pidfile} --environment=#{self.env}"
+      self.env_vars ||= {}
+      self.cmd_start ||= "bundle exec ./bin/rails s --port=#{self.port} --pid=#{self.pidfile} --environment=#{self.rails_env}"
     end
   end
 end
